c30ba1f5c732ced4bee26fbce61720803e5d1c2b
Adarsh <adarshniharika20@gmail.com>
2026-04-28 00:05:06 +0530

    UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Address_book
+﻿using System;
+
+namespace AddressBook
 {
-    internal class Program
+    class Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string Phone;
+        public string Email;
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Address Book");
         }
     }
 }
