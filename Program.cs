3b776a7b66a955f2260e2536f9ddd56d848a98d6
Adarsh <adarshniharika20@gmail.com>
2026-04-28 00:10:46 +0530

    UC2

--- Program.cs
+++ Program.cs
@@ -19,6 +19,35 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+
+            Contact contact = new Contact();
+
+            Console.Write("Enter First Name: ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            contact.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            contact.Address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            contact.City = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            contact.State = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            contact.Zip = Console.ReadLine();
+
+            Console.Write("Enter Phone: ");
+            contact.Phone = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            contact.Email = Console.ReadLine();
+
+            Console.WriteLine("\nContact Added Successfully:");
+            Console.WriteLine(contact.FirstName + " " + contact.LastName);
         }
     }
 }
